0504cf29b10def6dec48087e6fb52827277db5d5
Judy Wacker <judywacker@Judys-MBP-2.attlocal.net>
2019-03-06 14:12:11 -0800

    finish 18

--- Set3/18_implement_ctr_the_stream_cipher_mode.rb
+++ Set3/18_implement_ctr_the_stream_cipher_mode.rb
@@ -1,5 +1,14 @@
+require_relative "../Set1/07_aes_in_ecb_mode"
+
 class CTR
   def encrypt(message, key)
-    "hi"
+    keystream = ""
+    num_blocks = ((message.length - 1) / 16) + 1
+
+    num_blocks.times do |i|
+      keystream += aes_128_ecb_encrypt([0, i].pack('Q<*'), key, true)
+    end
+
+    ascii_xor(message, keystream)
   end
 end
